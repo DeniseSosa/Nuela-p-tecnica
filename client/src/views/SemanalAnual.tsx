ca1c7b22864eb22a877fb6093b115c33dffbaada
unknown <rocio.sosa94@gmail.com>
2024-05-27 21:06:01 -0300

    horas totales

--- client/src/views/SemanalAnual.tsx
+++ client/src/views/SemanalAnual.tsx
@@ -1,15 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { PropsProfe } from "../App";
 import axios from "axios";
 
 
 export const SemanalAnual: React.FC<PropsProfe> = ({profId}) => {
 
+  const [horasTotal,setHorasTotal]= useState <number>(0)
 const handlefilterSemanal = async() => {
   try {
     const response= await axios.get(`http://localhost:3001/cursos/profesores/${profId?.id}/semanal`)
     if(!response.data) throw new Error("sin datos")
-      return <div>{response.data}</div>
+      setHorasTotal(response.data)
+    console.log(horasTotal);
+    
   } catch (error) {
     console.log(error);
     
@@ -21,17 +24,18 @@ const handlefilterSemanal = async() => {
         <div className="flex justify-center">
         <div className="w-fit border-2 border-gray-100 rounded-xl bg-gray-100">
           <button 
-          className=" w-28 p-2 m-1 rounded-xl focus:outline-none focus:ring focus:ring-gray-300 focus:bg-gray-300">
+          className=" w-28 p-2 m-1 rounded-xl focus:outline-none focus:ring focus:ring-gray-300 focus:bg-gray-300"
+          onClick={handlefilterSemanal}>
             Semanal</button>
           <button 
           className=" w-28 p-2 m-1 rounded-xl focus:outline-none focus:ring focus:ring-gray-300 focus:bg-gray-300">
             Anual</button>
       </div>
       </div>
-
       <div className="flex flex-row">
       <div className="w-2/6 h-24 bg-gray-200 m-2 p-2 rounded-2xl"> Horas Totales 
-      </div>
+      <h2>{horasTotal}</h2></div>
+
       <div className="w-2/6 h-24 bg-gray-300 m-2 p-2 rounded-2xl"> Horas Lectivas
       </div>
       <div className="w-2/6 h-24 bg-gray-400 m-2 p-2 rounded-2xl">Horas Complementarias
